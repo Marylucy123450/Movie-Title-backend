1e981afa5d98f50444f5eded3b292c006f90d81e
Marylucy <marylucyatienoomenda.gmail.com>
2023-03-03 12:06:26 +0300

    defined the user added

--- my_project/app.rb
+++ my_project/app.rb
@@ -9,3 +9,11 @@ require 'json'
 
 # Configure the database connection
 set :database_file, 'config/database.yml'
+
+# Define the user and movie models
+class User < ActiveRecord::Base
+    has_many :movies
+    validates :email, presence: true, uniqueness: true
+    validates :password_hash, presence: true
+    validates :name, presence: true
+  end
